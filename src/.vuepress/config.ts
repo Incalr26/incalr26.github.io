e9f36bba4220745862762b000f093212b7e501cd
Incalr26 <incalr2025@outlook.com>
2026-08-08 12:34:34 +0800

    使用 Vuepress 搭建小站

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "Incalr26的小站",
+  description: "Incalr26的小站",
 
   theme,
 
